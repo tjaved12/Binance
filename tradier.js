e37674e90f85d6f7b334a86e3c23ed9b92a1ebfe
TJ <tahmeenaowais@yahoo.com>
2020-08-04 20:48:23 -0400

    fixed api and recent search

--- tradier.js
+++ tradier.js
@@ -1,9 +1,22 @@
-$(".my-sm-0").on("click",function(event){
+
+
+ //Declaring variables
+
+ $(".stock-tile").hide()
+ displayRecentSearchHistory() 
+ 
+ $("#searchBtn").on("click",function(event){
     event.preventDefault();
-     $("#symbolInfo").empty()
-    console.log("open")
+      console.log("open")
+    $(".card-title").empty()
+    $(".card-text").empty()
+    $(".btn-primary").empty()
+    $(".stock-tile").show()
 
     var symbol=  $("#inputSymbol").val()
+    getData(symbol)
+ })
+ function getData(symbol){
       var queryURL = "https://www.alphavantage.co/query?function=SYMBOL_SEARCH&keywords="+symbol+"&apikey=03SVGJ9SZRBGVOUS"
       $.ajax({
         url: queryURL,
@@ -14,10 +27,53 @@ $(".my-sm-0").on("click",function(event){
           // Log the queryURL
           console.log(queryURL);
     console.log(response)
-    $("#symbolInfo").append("<p> Symbol: " + (response.bestMatches[0]["1. symbol"]+ "</p>"))
+    $(".card-title").append(" Symbol: " + (response.bestMatches[0]["1. symbol"]))
+    $(".card-text").append(" Company Name: " + (response.bestMatches[0]["2. name"]))
+    $(".btn-primary").append(response.bestMatches[0]["1. symbol"])
+    
+       
 
-    $("#symbolInfo").append("<p> Company Name: " + (response.bestMatches[0]["2. name"]+ "</p>"))
-    
-    
 })
-})
+
+}
+$('#list').on('click', 'li', function(){
+  
+    console.log('hitt', $(this).text());
+    var symbol = $(this).text();
+    $(".card-title").empty()
+    $(".card-text").empty()
+    $(".btn-primary").empty()
+    $(".stock-tile").show()
+  
+   
+
+    console.log(symbol)
+    getData(symbol)
+    
+  
+  })
+  function displayRecentSearchHistory() {
+    $("#searchBtn").on("click",function(event){
+        event.preventDefault();
+    var symbol= $("#inputSymbol").val()
+    console.log(symbol)
+    //saving the symbol na
+    //Using parse to change the string into parse
+    var searchHistory = JSON.parse(localStorage.getItem('searchHistory')) || [];
+    // console.log('searchHistory', searchHistory)
+    //Declaring variables
+    //for loop to add scores in array
+    var ulEl = document.getElementById('list');
+$("#list").empty()
+    searchHistory.push(symbol);
+    localStorage.setItem("searchHistory", JSON.stringify(searchHistory));
+    var searchHistory = JSON.parse(localStorage.getItem('searchHistory')) || [];
+    for (var i = 0; i < searchHistory.length; i++) {
+      console.log('searchHistory', searchHistory)
+      var liEl = document.createElement('li')
+      // console.log('searchHistory', (searchHistory[i]));
+      liEl.textContent = (searchHistory[i]).toUpperCase();
+      ulEl.append(liEl)
+    }
+  }
+)}
