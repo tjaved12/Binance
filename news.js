90d6468d8b4e8d090d746e2bbc9844220967ae17
madhatter <matthew.rikard@gmail.com>
2021-02-15 18:23:39 -0500

    changed news api to increase rate limit

--- news.js
+++ news.js
@@ -1,39 +1,27 @@
 // api key e5d8f0f34fc44dc7a9c14e8e91146693
 
 function displayNews(){
-
-    var queryURL=' https://stocknewsapi.com/api/v1/category?section=general&items=50&token=4axewg16fegbjlsotq2ezkjzl1utnz8yf1vgblg6'+ '&apikey4axewg16fegbjlsotq2ezkjzl1utnz8yf1vgblg6';
-    
+    var queryURL='https://gnews.io/api/v4/top-headlines?token=6429df439c625744669b8f7c56a751ff';
     $.ajax({ 
         url: queryURL,
         method: "GET"
+    }).then(function(response){
+      console.log("queryURL = " + queryURL)
+      for (let i = 0; i < 5; i++) {
+        console.log(response)
+        console.log(response.articles[0].title)
 
-    }). then ( function (response ){
-        console.log (response)
-      console.log (queryURL)
-      
-for (let i = 0; i < 5; i++) {
-  console.log(response.data[i])
-
-
-
-
-  var card = `<div id="newsCard" class="card" style="width: 18rem,">\
-                <img src="${response.data[i].image_url}" class="card-img-top">\
-                <div class="card-body">\
-                  <a href="${response.data[i].news_url}" target="_blank">${response.data[i].title}</a>\
-                  <p class="card-text>${response.data[i].text}</p>\
-                  <news src="${response.data[i].news_url}">\
-                </div>\
-              </div>`
-
-  $("#news").append(card);
-}
-}
+        var card = `<div id="newsCard" class="card" style="width: 18rem,">\
+                      <img src="${response.articles[i].image}" class="card-img-top">\
+                      <div class="card-body">\
+                        <a href="${response.articles[i].url}" target="_blank">${response.articles[i].title}</a>\
+                        <p class="card-text>${response.articles[i].text}</p>\
+                        <news src="${response.articles[i].source.name}">\
+                      </div>\
+                    </div>`
+        $("#news").append(card);
+      }
+    }
 )}
-
-
-
-
 
 displayNews()
